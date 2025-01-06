2b56a7a2032adbc9501066e93a0b51e7c46aa08a
John Doe <johndoe@example.com>
2025-01-06 19:23:16 +0530

    feat: partially implement auth

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import AnimeAuthBG from "@/public/auth-anime_bg.jpg";
+import React, { ReactNode } from "react";
+
+interface AuthLayoutProps {
+  children: ReactNode;
+}
+
+function AuthLayout({ children }: AuthLayoutProps) {
+  return (
+    <main className="w-full h-screen grid place-items-center">
+      <div className="flex p-4 gap-x-2 bg-white w-full max-w-7xl rounded-[2.5rem]">
+        <section className="flex-1">{children}</section>
+        <section className="flex-1">
+          <Image
+            src={AnimeAuthBG}
+            className="object-cover rounded-[2.25rem] block h-[calc(100vh-10rem)] border-2 shadow-md"
+            alt="Anime Auth"
+          />
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default AuthLayout;

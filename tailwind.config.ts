eca1d6aafe55b9ef90b015f5ab85cbe5baafd051
Alish Baig <alishbaig2004@gmail.com>
2025-01-09 15:03:39 +0530

    feat: add daisyui package

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import DaisyUI from "daisyui";
 
 export default {
   content: [
@@ -13,5 +14,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [DaisyUI],
 } satisfies Config;

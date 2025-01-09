0b5c20c5556340fbe451288be26ac3a0e4831695
Alish Baig <alishbaig2004@gmail.com>
2025-01-09 20:54:46 +0530

    feat: enhance authentication flow with OAuth provider and form validation

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
-// import AnimeAuthBG from "@/public/cat-anime.webp";
-// import AnimeAuthBG from "@/public/cat-cute.webp";
-// import AnimeAuthBG from "@/public/cartoon-cat.webp";
-import AnimeAuthBG from "@/public/your-name (1).jpg";
+
+import AnimeAuthBG from "@/public/1358151.png";
 import React, { ReactNode } from "react";
 
 interface AuthLayoutProps {
@@ -12,12 +10,12 @@ interface AuthLayoutProps {
 function AuthLayout({ children }: AuthLayoutProps) {
   return (
     <main className="w-full h-screen grid place-items-center">
-      <div className="flex p-4 gap-x-2 bg-neutral w-full max-w-7xl rounded-[2.5rem]">
+      <div className="flex gap-x-2 bg-base-200 w-full max-w-7xl rounded-[2.5rem] overflow-hidden p-5 shadow-lg border-2 border-base-300">
         <section className="flex-1">{children}</section>
         <section className="flex-1">
           <Image
             src={AnimeAuthBG}
-            className="object-cover rounded-[2.25rem] block h-[calc(100vh-10rem)] border-2 shadow-md"
+            className="object-cover object-bottom block h-[calc(100vh-10rem)] shadow-md rounded-tl-md rounded-bl-md rounded-tr-[2.5rem] rounded-br-[2.5rem]"
             alt="Anime Auth"
           />
         </section>

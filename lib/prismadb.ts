946d8ad91e7337f5ac83281e7a2e74294b56f097
Alish Baig <alishbaig2004@gmail.com>
2025-01-09 15:04:37 +0530

    feat: add Prisma database setup and initial migration for authentication models

---
+++ lib/prismadb.ts
@@ -0,0 +1,26 @@
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
+import { PrismaClient } from "@prisma/client";
+
+// CREDIT: https://github.com/North-Hess/StickyNotes/blob/main/src/app/server/db.ts
+
+function primsaClientSingleton() {
+  const libSqlClient = createClient({
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_DATABASE_TOKEN!,
+  });
+
+  const libSQLAdapter = new PrismaLibSQL(libSqlClient);
+
+  return new PrismaClient({ adapter: libSQLAdapter });
+}
+
+const globalPrisma = globalThis as unknown as {
+  prisma: undefined | ReturnType<typeof primsaClientSingleton>;
+};
+
+const prismadb = globalPrisma.prisma ?? primsaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalPrisma.prisma = prismadb;
+
+export default prismadb;
